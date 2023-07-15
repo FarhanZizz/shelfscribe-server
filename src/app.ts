d04612d4e87261cf68c3ea322e2cd0da0577c63e
FarhanZizz <iamfarhanaziz@gmail.com>
2023-07-16 01:03:32 +0600

    added globar error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 //app.ts
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 const app: Application = express();
 import cors from "cors";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import httpStatus from "http-status";
+import cookieParser from "cookie-parser";
 
 app.use(cors());
+app.use(cookieParser());
 
 // parser
 app.use(express.json());
@@ -11,6 +15,23 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+//global error handler
+app.use(globalErrorHandler);
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API Not Found",
+      },
+    ],
+  });
+  next();
 });
 
 export default app;
